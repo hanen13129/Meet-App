226dff0fe3f927819b84a01129cb84b56def49d2
hanen13129 <benhassenhanen@gmail.com>
2021-11-24 20:44:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,42 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-
+import { getEvents, extractLocations } from './api';
 class App extends Component {
+   state = {
+    events: [],
+    locations: []
+  }
+   updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+      events :
+      events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <NumberOfEvents />
-        <EventList />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
       </div>
-    );
-  }
+  );
+}
+
 }
 export default App;
