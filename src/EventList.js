cb4ff91482085bd63a3ac09f89d194157d6aae46
hanen13129 <benhassenhanen@gmail.com>
2021-11-25 10:36:37 +0100

    Create EventList.js

---
+++ src/EventList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Event from './Event';
+import {Col} from 'react-bootstrap'
+
+class EventList extends Component {
+  render() {
+    const { events } = this.props;
+    return (
+      <ul className="EventList d-flex flex-row flex-wrap">
+ {events.map(event =>
+        <Col md={4} key={event.id} className="event-col">
+        <li>
+          <Event event={event} />
+        </li>
+        </Col>
+      )}
+      </ul>
+    );
+  }
+}
+
+export default EventList;
