224341e474117df131f32afc05385b69a7b7193c
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-12-07 21:28:28 +0100

    Update NumberOfEvents.test.js

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -2,21 +2,30 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
 
-describe('<NumberOfEvents /> component', () => {
-  let NumberOfEventsWrapper, numberOfEvents ;
-  beforeAll(() => {
-    NumberOfEventsWrapper = shallow(<NumberOfEvents numberOfEvents={numberOfEvents}/>)
-  });
-  test('render text input', () => {
-    expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
-  });
-  test('change state when text input changes', () => {
-    NumberOfEventsWrapper.setState({
-      numberOfEvents: 32
-    });
-    const eventObject = { target: { value: 22 }};
 
-    NumberOfEventsWrapper.find('.number-events-input').simulate('change', eventObject);
-    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(22);
+describe('<NumberOfEvents /> component', () => {
+  
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
   });
-})
+
+  test('render text input', () => {
+    expect(NumberOfEventsWrapper.find('.numberInput')).toHaveLength(1);
+  });
+
+  test('render text input correcty', () => {
+    const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
+    expect(NumberOfEventsWrapper.find('.numberInput').prop('value')).toBe(numberOfEvents);
+  });
+
+  // test('change state when text input changes', () => {
+  //   NumberOfEventsWrapper.setState({
+  //     numberOfEvents: '5'
+  //   });
+  //   const eventObject = { target: { value: '2' }};
+  //   NumberOfEventsWrapper.find('.numberInput').simulate('change', eventObject);
+  //   expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe('2');
+  // });
+
+});
