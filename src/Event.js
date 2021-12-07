8fa0f9ca9bf6d38fa5b9807f5464ffac1f002d8e
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-12-07 21:28:51 +0100

    Update Event.js

--- src/Event.js
+++ src/Event.js
@@ -1,39 +1,56 @@
-import React, { Component } from "react";
-import { Button} from 'react-bootstrap';
+import React, { Component } from 'react';
+import moment from 'moment';
+import { Button, Card } from 'react-bootstrap';
+import { mockData } from './mockData';
 
 class Event extends Component {
-  state = {
-    collapsed: true,
-  };
+  constructor(props) {
+		super(props);
+		this.state = {
+			opened: false,
+      mockData: '',
+      event: {}
+		};
+		this.toggleBox = this.toggleBox.bind(this);
+  }
+  
+  toggleBox() {
+		const { opened } = this.state;
+		this.setState({
+			opened: !opened,
+		});
+	}
 
-  handleClick = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
+  mockData = JSON.parse(JSON.stringify(mockData));
 
   render() {
-    const { event } = this.props;
-    const { collapsed } = this.state;
-    return  <div className="event">
-              <h2 className="summary">{event.summary}</h2>
-              <p className="start-date">
-              {event.start.dateTime} ({event.start.timeZone})
-              </p>
-              <p className="location">
-                {event.location}
-              </p>
-              <Button variant="primary" className="show-more" onClick={this.handleClick}>
-                    Toggle details
-              </Button>
-                <div className={`more-info ${collapsed ? `hidden` : `show`}`}>
-                  <h3>About the event:</h3>
-                  <a href={event.htmlLink} rel="noreferrer" target="_blank">
-                    See details on Google Calendar
-                  </a>
-                  <p className="event-description">{event.description}</p>
-                </div>
-            </div>
+    var { btnText, event } = this.props;
+		const { opened } = this.state;
+    const eventStart = moment(event.start.dateTime).format('MMMM Do YYYY, h:mm a');
+
+		if (opened) {
+			btnText ='Hide Details';
+		} else {
+			btnText ='View Details';
+		}
+    return (
+      <Card>
+        <Card.Body>
+          <Card.Title>{event.summary} </Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{`${eventStart}`}</Card.Subtitle>
+          <Card.Text>{event.location}</Card.Text>
+          <div className="ToggleButtonWrapper">
+            <Button variant="link" className="ToggleButton" onClick={this.toggleBox}>{btnText}</Button>
+          </div>
+          {opened && (					
+            <Card.Text className="eventDetails">
+              {event.description}
+            </Card.Text>
+            )}
+        </Card.Body>
+      </Card>
+    );
   }
 }
+
 export default Event;
