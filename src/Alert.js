3a6546b1053708b1c5fa09abfb8be55c00f379f1
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-12-07 21:28:24 +0100

    Update Alert.js

--- src/Alert.js
+++ src/Alert.js
@@ -1,41 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-// the main parent class
 class Alert extends Component {
   constructor(props) {
     super(props);
-    this.color = null;
-  }
+      this.color = null;
+    }
 
-  getStyle = () => {
-    return {
-      color: this.color,
-    };
-  }
+    getStyle = () => {
+      return {
+        color: this.color,
+      };
+    }
 
-  render() {
-    return (
-      <div className="Alert">
-        <p style={this.getStyle()}>{this.props.text}</p>
-      </div>
-    );
-  }
+    render() {
+      return (
+        <div className="Alert">
+          <p style={this.getStyle()}>{this.props.text}</p>
+        </div>
+      );
+    }
 }
 
- // the  infaAlert subclass
+//Alert for no city match
 class InfoAlert extends Alert {
   constructor(props) {
     super(props);
-    this.color = 'blue';
+    this.color = 'goldenrod';
+  }
+  getStyle = () => {
+    return {
+      color: this.color,
+      background: '#fff',
+      margin: '0px',
+      padding: '0px 10px',
+    };
   }
 }
 
- // the  erorAlert subclass
- class ErrorAlert extends Alert {
+//Alert for incorrect number input
+class ErrorAlert extends Alert {
   constructor(props) {
     super(props);
     this.color = 'red';
   }
 }
 
-export { InfoAlert, ErrorAlert };
+//Alert for no network connection
+class NetworkAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = '#f8a01f';
+  }
+}
+
+export { InfoAlert, ErrorAlert, NetworkAlert };
