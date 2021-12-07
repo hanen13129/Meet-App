d3b9dc5d3ae04db1c7cf8302d64d1f423c84528d
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-12-07 21:28:56 +0100

    Update filterEventsByCity.test.js

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,66 +1,49 @@
-import { loadFeature, defineFeature } from 'jest-cucumber';
-
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../App';
-import { extractLocations } from '../api';
-import { mockData } from '../mock-data';
 import CitySearch from '../CitySearch';
+import { extractLocations } from '../api';
+import { mockData } from '../mockData';
+
+import { loadFeature, defineFeature } from 'jest-cucumber';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
 defineFeature(feature, test => {
+  test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
+    given('user hasn’t searched for any city', () => {
 
+    });
 
+    let AppWrapper;
+    when('the user opens the app', () => {
+      AppWrapper = mount(<App />)
+    });
 
-// Feature file has a scenario titled "When user hasn’t searched for a city, show upcoming events from all cities.", but no match found in step definitions. Try adding the following code:
-
-test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
-  given('user hasn’t searched for any city', () => {
-
-  });
-  let AppWrapper;
-  when('the user opens the app', () => {
-    AppWrapper = mount(<App />);
-  });
-
-  then('the user should see the list of upcoming events.', () => {
-    AppWrapper.update();
-    expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
-  });
+    then('the user should see the list of upcoming events.', () => {
+      AppWrapper.update();
+      expect(AppWrapper.find('.EventItem').hostNodes()).toHaveLength(mockData.length);
+    });
 });
-
-
-
-
-
-// Feature file has a scenario titled "User should see a list of suggestions when they search for a city", but no match found in step definitions. Try adding the following code:
 
 test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
-  let CitySearchWrapper;
-  const locations = extractLocations(mockData);
-  given('the main page is open', () => {
-    CitySearchWrapper = shallow(<CitySearch updateEvents={() => {}} locations={locations} />);
-  });
+    let CitySearchWrapper;
+    const locations = extractLocations(mockData);
+    given('the main page is open', () => {
+      CitySearchWrapper = shallow(<CitySearch updateEvents={() => {}} locations={locations} />);
+    });
 
-  when('the user starts typing in the city textbox', () => {
-    CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
-  });
+    when('the user starts typing in the city textbox', () => {
+      CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
+    });
 
-  then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-    expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2);
-  });
+    then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
+      expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2);
+    });
 });
 
-
-
-
-
-
-// Feature file has a scenario titled "User can select a city from the suggested list", but no match found in step definitions. Try adding the following code:
-
 test('User can select a city from the suggested list', ({ given, and, when, then }) => {
-  let AppWrapper;
+    let AppWrapper;
     given('user was typing “Berlin” in the city textbox', async () => {
       AppWrapper = await mount(<App />);
       AppWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
@@ -70,17 +53,18 @@ test('User can select a city from the suggested list', ({ given, and, when, then
       AppWrapper.update();
       expect(AppWrapper.find('.suggestions li')).toHaveLength(2);
     });
+
     when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
       AppWrapper.find('.suggestions li').at(0).simulate('click');
     });
-
+    
     then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
       const CitySearchWrapper = AppWrapper.find(CitySearch);
       expect(CitySearchWrapper.state('query')).toBe('Berlin, Germany');
     });
-
+    
     and('the user should receive a list of upcoming events in that city', () => {
-      expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
+      expect(AppWrapper.find('.EventItem')).toHaveLength(mockData.length);
     });
 });
 });
