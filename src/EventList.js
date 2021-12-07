8fc01e925c8b0b71d3af86fb3d5a119f9fe4f758
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-12-07 21:28:48 +0100

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import Event from './Event';
-import {Col} from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap';
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
-      <ul className="EventList d-flex flex-row flex-wrap">
- {events.map(event =>
-        <Col md={4} key={event.id} className="event-col">
-        <li>
-          <Event event={event} />
-        </li>
+      <Row className="mt-3">
+        <Col md={12}>
+          <ul className="EventList">
+            {events && events.map(event =>
+              <li key={event.id} className="EventItem">
+                <Event event={event} />
+              </li>
+            )}
+          </ul>
         </Col>
-      )}
-      </ul>
+      </Row>
     );
   }
 }
