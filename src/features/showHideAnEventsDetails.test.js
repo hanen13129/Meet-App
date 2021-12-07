aa81b1ec2a270b36908e7db8576b91c6ec912e65
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-12-07 21:29:17 +0100

    Update showHideAnEventsDetails.test.js

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,69 +1,65 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
-
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import App from '../App';
-import Event from '../Event.js';
+import { mockData } from '../mockData';
+import EventList from '../EventList';
+import Event from '../Event';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
+  
+  let EventListWrapper;
+  let EventWrapper;
+  let AppWrapper;
+  test('An event element is collapsed by default', ({ given, when, then }) => {
+    given('the user hasn’t expanded an event to see its details', () => {
+      EventListWrapper = mount(<EventList events={mockData} />);
+      EventWrapper = mount(<Event event={mockData[0]} />);
+    });
 
-  test('An event element is collapsed by default.', ({ given, when, then }) => {
-    let AppWrapper;  
-    given('a user is on the main page,', () => {
+    when('the user opens the app', () => {
       AppWrapper = mount(<App />);
+    });
 
-      });
-
-      when('an event is displayed', () => {
-        expect(AppWrapper.find('.event')).toBeDefined();
-      });
-
-      then('event details will be collapsed.', async () =>  {
-        const eventDetails = await AppWrapper.find('.event .more-info');
-      expect(eventDetails.find('.hide')).toBeDefined();
-      });
+    then('the user should see a list of all the events without details', () => {
+      expect(EventWrapper.find(".eventDetails")).toHaveLength(0);
+    });
   });
 
-  test('User can expand an event to see its details.', ({ given, when, then }) => {
-    let AppWrapper;  
+  test('User can expand an event to see its details', ({ given, when, then }) => {
+    given('the main page is open', () => {
+      AppWrapper = mount(<App />);
+      EventListWrapper = mount(<EventList events={mockData} />);
+      EventWrapper = mount(<Event event={mockData[0]} />);
+    });
 
-      given('a user has list of events', () => {
-        AppWrapper = mount(<App />);
-      });
+    when('the user clicked on expand/more details button on a specific event', () => {
+      EventWrapper.find(".ToggleButton").at(1).simulate("click");
+    });
 
-      when('the user clicks on an individual event', () => {
-        AppWrapper.update();
-        let Event = AppWrapper.find('.event');
-        Event.find('.show-more').at(0).simulate('click');
-      });
-
-      then('the event details will be displayed.', () => {
-        const EventDetails =  AppWrapper.find('.event .more-info');
-        expect(EventDetails.find('.show')).toBeDefined();
-      });
+    then('the user should see the details of this specific event', () => {
+      expect(EventWrapper.find(".eventDetails")).toHaveLength(2);
+    });
   });
 
-  test('User can collapse an event to hide its details.', ({ given, when, then }) => {
-    let AppWrapper; 
-    AppWrapper = mount(<App />);
+  test('User can collapse an event to hide its details', ({ given, when, then }) => {
+    given('an event’s details is expanded', () => {
+      AppWrapper = mount(<App />);
+      EventListWrapper = mount(<EventList events={mockData} />);
+      EventWrapper = mount(<Event event={mockData[0]} />);
+      EventWrapper.find(".ToggleButton").at(1).simulate("click");
+      EventWrapper.find(".eventDetails");
+    });
 
-      given('a user has expanded an individual event,', () => {
-        const EventDetails =  AppWrapper.find('.event .more-info');
-        expect(EventDetails.find('.show')).toBeDefined();
-      });
+    when('the user clicks on the collapse/show less button', () => {
+      EventWrapper.find(".ToggleButton").at(1).simulate("click");
+    });
 
-      when('a user has clicked “minimize”', () => {
-        AppWrapper.update();
-        let Event = AppWrapper.find('.event');
-        Event.find('.show-more').at(0).simulate('click');
-      });
-
-      then('the event details will be hidden.', () => {
-        const EventDetails =  AppWrapper.find('.event .more-info');
-        expect(EventDetails.find('.hidden')).toBeDefined();
-      });
+    then('the user should no longer see the event’s details', () => {
+      expect(EventWrapper.find(".eventDetails")).toHaveLength(0);
+    });
   });
 
-});
+})
